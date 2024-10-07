b79e1c7c632c7a0e2f7e49d98cc829d9aa93d958
Ryan Hinkle <ryanhinkle@Ryans-MacBook-Air-4.local>
2024-10-07 01:39:48 -0500

    Added favicon and aesthetic changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom';
 function Home({ links }) {
   return (
     <main className="min-h-screen bg-neutral-900 p-8 flex flex-col items-center">
-      <h1 className="text-red-700 text-3xl font-extrabold mb-8">ECaMS Billboard</h1>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <h1 className="text-red-700 text-3xl font-extrabold mb-8" style={{ marginRight: '10px' }}>ECaMS Billboard</h1>
+        <img src="lewis_logo.png" alt="Lewis Title Logo" style={{ width: '50px', height: 'auto', marginTop: '-30px' }} />
+      </div>
       <nav className="w-full max-w-md flex flex-col gap-4">
         {links.map((link, index) => (
           <Link

a32c8e433d9003d467b5519fe537aac8531b2693
mszost <mszo@tuta.io>
2024-10-11 13:36:14 -0500

    remove redundant files in /public, undo rename of BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Upload from './pages/Upload';
@@ -21,7 +21,7 @@ function App() {
   // Defines page routing scheme.
   // For example, going to `<URL>/about` will return `./pages/About.js`.
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home links={links}/>}/>
         <Route path="/upload" element={<Upload/>}/>
@@ -30,7 +30,7 @@ function App() {
         <Route path="/about" element={<About/>}/>
         <Route path="/credits" element={<Credits/>}/>
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 

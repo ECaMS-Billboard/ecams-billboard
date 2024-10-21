067be00f02df8e7946719783ae05d1d431768784
mszost <mszo@tuta.io>
2024-10-20 19:26:30 -0500

    convert inline CSS styles to tailwind

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 function Home({ links }) {
   return (
     <main className="min-h-screen bg-neutral-900 p-8 flex flex-col items-center">
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <h1 className="text-red-700 text-3xl font-extrabold mb-8" style={{ marginRight: '10px' }}>ECaMS Billboard</h1>
-        <img src="lewis_logo.png" alt="Lewis Title Logo" style={{ width: '50px', height: 'auto', marginTop: '-30px' }} />
+
+      <div className="flex center items-center">
+        <h1 className="text-red-700 text-3xl font-extrabold mb-8 -translate-x-2">ECaMS Billboard</h1>
+        <img className="w-12 -translate-y-4" src="lewis_logo.png" alt="Lewis Logo"/>
       </div>
+
       <nav className="w-full max-w-md flex flex-col gap-4">
         {links.map((link, index) => (
           <Link

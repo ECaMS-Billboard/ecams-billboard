17e031d1f3dbda7c13a1192ffa69f3f0b9c11e9c
justinc098 <100169881+justinc098@users.noreply.github.com>
2024-10-20 20:01:33 -0500

    Update Contact.js
    Add the email to contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,10 +2,10 @@ function Contact() {
   return (
     <div className="min-h-screen bg-neutral-900 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md mx-auto">
-        <h1 className="text-red-700 text-3xl font-bold mb-4">Contact</h1>
-          <p className="text-gray-300">This is the contact page.</p>
-        </div>
+        <h1 className="text-red-700 text-3xl font-bold mb-4">Contact Us</h1>
+        <button onClick={() => window.location = 'mailto:ecamsbb@gmail.com'}>ecamsbb@gmail.com</button>
       </div>
+    </div>
   );
 }
 

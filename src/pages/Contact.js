92f0fce1a574a9a48a78557206a43c61b55bdad8
justinc098 <100169881+justinc098@users.noreply.github.com>
2024-10-20 22:38:41 -0500

    Make Email look like a link

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,7 @@ function Contact() {
     <div className="min-h-screen bg-neutral-900 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md mx-auto">
         <h1 className="text-red-700 text-3xl font-bold mb-4">Contact Us</h1>
-        <button onClick={() => window.location = 'mailto:ecamsbb@gmail.com'}>ecamsbb@gmail.com</button>
+        <a className="underline text-blue-500" href="mailto:ecamsbb@gmail.com">ecamsbb@gmail.com</a>.
       </div>
     </div>
   );

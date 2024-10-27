e993377b8112094468f2d8e978fc582804c12393
mszost <mszo@tuta.io>
2024-10-27 13:50:44 -0500

    add correct phone numbers, improve styling in Resources.js

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -1,28 +1,28 @@
 const resourcesData = [
     {
-      title: 'Student Admissions',
-      phone: '123-456-7890',
-      email: 'admissions@example.com',
-    },
-    {
-      title: 'Campus Police',
-      phone: '098-765-4321',
-      email: 'police@example.com',
-    },
-    {
-      title: 'Health and Wellness Center',
-      phone: '555-123-4567',
-      email: 'wellness@example.com',
+      title: 'IT Support',
+      phone: '(815) 836-5950',
+      email: 'servicedesk@lewisu.edu'
     },
     {
       title: 'Financial Aid',
-      phone: '444-555-6666',
-      email: 'financialaid@example.com',
+      phone: '(815) 836-5263',
+      email: '',
     },
     {
       title: "Registrar's Office",
-      phone: '777-888-9999',
-      email: 'registrar@example.com',
+      phone: '(815) 836-5133',
+      email: '',
+    },
+    {
+      title: 'Admissions',
+      phone: '(815) 836-5250',
+      email: '',
+    },
+    {
+      title: 'LUPD',
+      phone: '(815) 836-5222',
+      email: '',
     },
   ];
   
@@ -32,12 +32,12 @@ const resourcesData = [
         <h1 className="text-red-700 text-3xl font-bold mb-4">Resources</h1>
         {resourcesData.map((resource, index) => (
           <div key={index} style={{ marginBottom: '20px' }}>
-            <h2 className="text-gray-300">{resource.title}</h2>
-            <p className="underline text-blue-500">
-              Phone: <a href={`tel:${resource.phone}`}>{resource.phone}</a>
+            <h2 className="text-white font-bold">{resource.title}</h2>
+            <p className="text-gray-300">
+              Phone: <a className="underline text-blue-500" href={`tel:${resource.phone}`}>{resource.phone}</a>
             </p>
-            <p className="underline text-blue-500">
-              Email: <a href={`mailto:${resource.email}`}>{resource.email}</a>
+            <p className="text-gray-300">
+              Email: <a className="underline text-blue-500" href={`mailto:${resource.email}`}>{resource.email}</a>
             </p>
           </div>
         ))}

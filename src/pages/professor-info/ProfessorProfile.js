2d960853c0f29204e685d752a3cc5fcded25adea
mszost <mszo@tuta.io>
2024-11-21 20:55:50 -0600

    add getColorFromRmpScore function in ProfessorProfile.js

--- src/pages/professor-info/ProfessorProfile.js
+++ src/pages/professor-info/ProfessorProfile.js
@@ -1,11 +1,17 @@
 import { useParams } from 'react-router-dom';
 
+// normalizes score and returns color category
+const getColorFromRmpScore = (score)  => {
+  const colors = ['#f55c5c', '#f06f44', '#e5832c', '#d49614', '#bea801', '#a3b816', '#82c636', '#55d259'];
+  return colors[Math.min(Math.floor((score-1) / (5-1) * (colors.length -1)), colors.length -1)];
+}
+
 
 function ProfessorProfile() {
     const { id } = useParams();
 
     return (
-      <div>
+      <div style={{color: getColorFromRmpScore(5.0)}}>
         {id}
       </div>
     );

d183409226472af5722be76410e93890c82dcc67
AirlineExpress <samueljswedo@lewisu.edu>
2024-11-26 22:49:04 -0600

    updated navbar to a popup

--- src/pages/CapstoneProducts.js
+++ src/pages/CapstoneProducts.js
@@ -1,9 +1,12 @@
+import Navigationbar from '../components/Navigationbar';
+
 const CapstoneProducts = () => {
     const quizmaster = "https://quizmaster-c66a2.web.app/home"
   
     return (
         <body className="min-h-screen bg-neutral-900 p-8 flex flex-col items-center">
-            <div className="max-w-xl mx-auto">
+            <Navigationbar />
+            <div className="max-w-xl mx-auto mt-10">
                 <div className='text-center'>
                     <h1 className="text-red-500 text-3xl font-bold mb-4">Capstone Products</h1>
                     </div >

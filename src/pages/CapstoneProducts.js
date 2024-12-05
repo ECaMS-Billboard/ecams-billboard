08af486f108826145c9de683a6b2a65c0042f3d7
TwiceBaked <gabrieljcarlson@lewisu.edu>
2024-12-05 13:56:57 -0600

    completed stories 15.08 and 15.12

--- src/pages/CapstoneProducts.js
+++ src/pages/CapstoneProducts.js
@@ -8,20 +8,23 @@ const CapstoneProducts = () => {
             <Navigationbar />
             <div className="max-w-xl mx-auto mt-10">
                 <div className='text-center'>
-                    <h1 className="text-red-500 text-3xl font-bold mb-4">Capstone Products</h1>
+                    <h1 className="text-red-500 text-3xl font-bold mb-4 underline">Capstone Products</h1>
                     </div >
                         <div className="text-gray-300 text-center">
                             Here you can check out what other Capstone teams have created.
-                        <h1 className="text-red-500 text-2xl font-bold mb-2">
+                        <h1 className="text-red-500 text-2xl font-bold mb-2 mt-2">
                             QuizMaster: an alternative to Quizlet
                         </h1>
-                        <p>
-                            Follow the link below and login using Google to utilize QuizMaster.
-                        </p>
+                        
                         <a href={quizmaster} target="_blank" rel="noopener noreferrer">
-                            <button className="underline text-blue-400 font-bold mb-2">Try QuizMaster</button>
+                            <img src="QuizMasterFlyer.png" alt="app store button" className="mb-4"/>
+                            <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Try QuizMaster</button>
                         </a>
                         
+                        <h1 className="text-red-500 text-2xl font-bold mt-10">
+                            More Capstone Products Coming Soon.
+                        </h1>
+
                         </div>
                 </div>
             </body>

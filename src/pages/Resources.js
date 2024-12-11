5b6e7f15a0e0fc65832102e853e1bd9d73860e22
AirlineExpress <samueljswedo@lewisu.edu>
2024-12-10 19:50:05 -0600

    anthony updated resources and proflist for search and additional information

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -5,32 +5,44 @@ const resourcesData = [
     {
       title: 'IT Support',
       phone: '(815) 836-5950',
-      email: 'servicedesk@lewisu.edu'
+      email: 'servicedesk@lewisu.edu',
+      description: 'Provides assistance with technical issues including account access, software, and hardware problems',
     },
     {
       title: 'Financial Aid',
       phone: '(815) 836-5263',
       email: 'finaid@lewisu.edu',
+      description: 'Helps students understand and manage their financial aid packages, scholarships, and loans',
     },
     {
       title: "Registrar's Office",
       phone: '(815) 836-5133',
       email: 'registrar',
+      description: 'Manages student records, course registration, and academic transcripts',
     },
     {
       title: 'Admissions',
       phone: '(815) 836-5250',
       email: 'admissions@lewisu.edu',
+      description: 'Handles applications, campus visits, and enrollment processes for prospective students',
     },
     {
       title: 'LUPD Non-Emergency',
       phone: '(815) 836-5222',
       email: '',
+      description: '24 hours a day, 7 days a week, year round',
     },
     {
       title: 'LUPD Emergency',
       phone: '(815) 836-5911',
       email: '',
+      description: '24 hours a day, 7 days a week, year round',
+    },
+    {
+      title: 'Student Wellness Center',
+      phone: '(815) 836-5455',
+      email: '',
+      description: 'Offers health and counseling services to support student well-being and mental health. Hours are as follows: Fall and Spring, Monday-Friday, 8:30am - 5pm, and Summer Tuesday-Wenesday, 8:30am - 5pm',
     },
   ];
   //Display everything
@@ -39,7 +51,7 @@ const resourcesData = [
       <body className="min-h-screen bg-neutral-900 p-8 flex flex-col items-center">
         <Navigationbar/>
         <h1 className="text-red-500 text-3xl font-bold mb-4 mt-10">Resources</h1>
-        <div>
+        <div className="max-w-xl mx-auto">
           {resourcesData.map((resource, index) => (
             <div key={index} style={{ marginBottom: '20px' }}>
               <h2 className="text-white font-bold">{resource.title}</h2>
@@ -48,6 +60,9 @@ const resourcesData = [
               </p>
               <p className="text-gray-300">
                 Email: <a className="underline text-blue-500" href={`mailto:${resource.email}`}>{resource.email}</a>
+              </p>
+              <p className="text-gray-300">
+                Description: {resource.description}
               </p>
             </div>
           ))}

2c8d87efd94d182e57af2c539a6657f5b5d8ddeb
AirlineExpress <samueljswedo@lewisu.edu>
2025-02-03 19:09:51 -0600

    Updated Navigation Bar

--- src/components/Navigationbar.js
+++ src/components/Navigationbar.js
@@ -3,15 +3,8 @@ import { Link } from 'react-router-dom';
 
 const Navigationbar = () => {
 
-  const [popupStatus, setPopupStatus] = useState(false);
-  const togglePopup = () => {setPopupStatus(!popupStatus);}
   return (
-    <nav className={`p-1 w-full fixed top-0 left-0 ${popupStatus ? 'bg-red-700' : ''}`}>
-      <div className="flex flex-start justify-start">
-        <button onClick={togglePopup} className="bg-white text-red-700 px-4 py-2 rounded mx-2 opacity-50 hover:opacity-100">三</button>
-
-        {popupStatus && (
-          <div className="flex flex-wrap justify-center items-center text-center w-full">
+          <div className="flex flex-wrap justify-center items-center text-center w-full bg-red-900 w-full fixed top-0">
             <Link to="/" className="text-gray-300 hover:text-white mx-2 flex-grow text-center">Home</Link>
             <Link to="/Slides" className="text-gray-300 hover:text-white mx-2 flex-grow text-center">Slideshow</Link>
             <Link to="/Upload" className="text-gray-300 hover:text-white mx-2 flex-grow text-center">Upload</Link>
@@ -20,9 +13,6 @@ const Navigationbar = () => {
             <Link to="/Resources" className="text-gray-300 hover:text-white mx-2 flex-grow text-center">Resources</Link>
             <Link to="/Capstoneproducts" className="text-gray-300 hover:text-white mx-2 flex-grow text-center">Capstone</Link>
           </div>
-        )}
-      </div>
-    </nav>
   );
 };
 

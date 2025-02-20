4e6669ce556fc0d55814356d05de13b480c2bce6
AirlineExpress <123339883+AirlineExpress@users.noreply.github.com>
2025-02-19 23:00:26 -0600

    Added A's changes

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -6,25 +6,29 @@ const resourcesData = [
       title: 'IT Support',
       phone: '(815) 836-5950',
       email: 'servicedesk@lewisu.edu',
-      description: 'Provides assistance with technical issues including account access, software, and hardware problems.',
+      description: 'Provides assistance with technical issues including account access, software, and hardware problems',
+      hours: 'Monday-Thursday: 7:30am-10pm Friday: 7:30am-5pm Saturday:9am-5pm Sunday:1:30pm-10pm.',
     },
     {
       title: 'Financial Aid',
       phone: '(815) 836-5263',
       email: 'finaid@lewisu.edu',
-      description: 'Helps students understand and manage their financial aid packages, scholarships, and loans.',
+      description: 'Helps students understand and manage their financial aid packages, scholarships, and loans',
+      hours: 'Monday-Friday: 8:30am-5:00pm.',
     },
     {
       title: "Registrar's Office",
       phone: '(815) 836-5133',
       email: 'registrar',
-      description: 'Manages student records, course registration, and academic transcripts.',
+      description: 'Manages student records, course registration, and academic transcripts',
+      hours: 'Monday-Friday: 8:30am-5:00pm.',
     },
     {
       title: 'Admissions',
       phone: '(815) 836-5250',
       email: 'admissions@lewisu.edu',
-      description: 'Handles applications, campus visits, and enrollment processes for prospective students.',
+      description: 'Handles applications, campus visits, and enrollment processes for prospective students',
+      hours: 'Monday-Friday: 8:30am to 5:00pm.',
     },
     {
       title: 'LUPD Non-Emergency',
@@ -42,7 +46,8 @@ const resourcesData = [
       title: 'Student Wellness Center',
       phone: '(815) 836-5455',
       email: '',
-      description: 'Offers health and counseling services to support student well-being and mental health. Hours are as follows: Fall and Spring, Monday-Friday, 8:30am - 5pm, and Summer Tuesday-Wenesday, 8:30am - 5pm.',
+      description: 'Offers health and counseling services to support student well-being and mental health.',
+      hours: 'Hours are as follows: Fall and Spring, Monday-Friday, 8:30am-5pm, and Summer Tuesday-Wenesday, 8:30am-5pm.',
     },
   ];
   //Display everything
@@ -63,6 +68,9 @@ const resourcesData = [
               </p>
               <p className="text-gray-300">
                 Description: {resource.description}
+              </p>
+              <p className="text-gray-300">
+                Office Hours: {resource.hours}
               </p>
             </div>
           ))}
